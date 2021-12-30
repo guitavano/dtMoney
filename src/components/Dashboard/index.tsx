35edbbdfa835b2da93445de40736563103053958
guilhermetavanorocky <guilherme.santos@rocky.ag>
2021-12-30 17:23:03 -0300

    add transactions table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,12 +1,12 @@
 import { Container } from "./styles";
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 
 export function Dashboard(){
     return(
-        <>
-            <Container>
-                <Summary />
-            </Container>
-        </>
+        <Container>
+            <Summary />
+            <TransactionsTable />
+        </Container>
     );
 }

4c103faa03f2d0ca80a38672d5a964e0b29a5bce
guitavano <tavano62@gmail.com>
2022-01-10 22:10:28 -0300

    insert createTransaction in Context

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -1,8 +1,6 @@
 import { createContext, useEffect, useState, ReactNode } from "react";
 import { api } from "./services/api";
 
-export const TransactionsContext = createContext<Transaction[]>([]);
-
 interface Transaction {
     id: number;
     title: string;
@@ -12,20 +10,34 @@ interface Transaction {
     createdAt: string;
 }
 
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
 interface TransactionsProviderProps {
     children: ReactNode
 }
 
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData
+);
+
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([])
-
     useEffect(() => {
         api.get('/transactions')
             .then(response => setTransactions(response.data.transactions))
     }, [])
 
+    function createTransaction(transaction: TransactionInput) {
+        api.post('/transactions', transaction)
+    }
+
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{ transactions, createTransaction }}>
             {children}
         </TransactionsContext.Provider>
     )

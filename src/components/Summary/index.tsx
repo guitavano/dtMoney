d217c1ccd1b68ebd6da768f56ff814c86f196e16
guitavano <tavano62@gmail.com>
2022-01-11 22:05:11 -0300

    create useTransactions hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,11 +3,11 @@ import { Container } from "./styles"
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
+import { useTransactions } from '../../hooks/useTransactions'
 
-import { TransactionsContext } from '../../TransactionsContext'
 
 export function Summary() {
-    const { transactions } = useContext(TransactionsContext)
+    const { transactions } = useTransactions()
 
     // const totalDeposits = transactions.reduce((acc, transaction) => {
     //     if (transaction.type === 'deposit') {
